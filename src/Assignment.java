1f13387dc38d007a9af1df065c77456ad03df5c0
e235744 <e235744@ie.u-ryukyu.ac.jp>
2023-12-12 21:42:23 +0900

    second commit

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,7 +1,13 @@
 public class Assignment {
     public static void main(String[] args){
         String str = null;
-        System.out.println(str.length());
+
+        try {
+            System.out.println(str.length());
+        }catch(java.lang.NullPointerException e){
+            System.out.println("java.lang.NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
 
